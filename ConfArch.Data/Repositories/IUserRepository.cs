2a1e82bcc01511afec099f7479d296802fda9207
Fáusio <luisfausio@gmail.com>
2021-09-16 21:48:54 +0200

    account controller

---
+++ ConfArch.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfArch.Data.Repositories
+{
+  public  interface IUserRepository
+    {
+        public User GEtBByUserNameAndPassword(string username, string password)
+        {
+
+        }
+    }
+}
---
+++ ConfArch.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using ConfArch.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfArch.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public  IActionResult  Login(string returnUrl = "/") =>    View(new LoginModel { ReturnUrl = returnUrl });
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = userre
+
+            return    null;
+        }
+
+    }
+}
