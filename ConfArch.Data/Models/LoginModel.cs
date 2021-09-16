3008b1d7fea9522b6236884f796585209a21095f
Fáusio <luisfausio@gmail.com>
2021-09-16 21:48:40 +0200

    Login model

---
+++ ConfArch.Data/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfArch.Data.Models
+{
+   public class LoginModel
+    {
+        public string UserName   { get; set; }
+        public string PassWord { get; set; }
+        public bool RememberLogin { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
