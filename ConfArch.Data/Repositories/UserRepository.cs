12e578116371e6c37f0a3de69795f511eb9239ec
Fáusio <luisfausio@gmail.com>
2021-09-17 07:15:35 +0200

    UserRepository

---
+++ ConfArch.Data/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using ConfArch.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConfArch.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private List<User> users = new List<User>
+        {
+            new User { Id = 3522, Name = "roland", Password = "A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ=",
+                FavoriteColor = "blue", Role = "Admin", GoogleId = "101517359495305583936" }
+        };
+
+        public User GetByUsernameAndPassword(string username, string password)
+        {
+            var user = users.SingleOrDefault(u => u.Name == username &&  u.Password == password.Sha256());
+              return user;
+        }
+         
+    }
+}
